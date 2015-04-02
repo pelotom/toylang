08f2afcf0d36f82238a37ecaacd0e27a2c00ddf4
Tom Crockett <pelotom@gmail.com>
2015-04-02 01:45:36 -0700

    Initial commit

---
+++ src/Expressions.purs
@@ -0,0 +1,51 @@
+module Toy.Expression where
+
+import Data.Tuple
+import Data.Foldable
+
+type Id = String
+
+data Expr = Var Id
+          | Abs Id Expr
+          | App Expr Expr
+
+instance eqExpr :: Eq Expr where
+  (==) (Var x) (Var y) = x == y
+  (==) (Abs x1 e1) (Abs x2 e2) = x1 == x2 && e1 == e2
+  (==) (App a1 b1) (App a2 b2) = a1 == a2 && b1 == b2
+  (/=) e1 e2 = not $ e1 == e2
+
+instance showExpr :: Show Expr where
+  show = pretty
+
+pretty (Var x) = x
+pretty e@(Abs _ _) = case stripBoundVars e of
+    Tuple xs e' -> "\\" ++ intercalate " " xs ++ " -> " ++ pretty e'
+  where
+    stripBoundVars :: Expr -> Tuple [Id] Expr
+    stripBoundVars (Abs x e) = case stripBoundVars e of
+      Tuple xs e' -> Tuple (x : xs) e'
+    stripBoundVars e = Tuple [] e
+    
+pretty (App e1 e2) = paren true e1 ++ " " ++ paren false e2
+  where
+    paren _ (Var x) = x
+    paren true e@(App _ _) = pretty e
+    paren _ e = "(" ++ pretty e ++ ")"
+
+isValue :: Expr -> Boolean
+isValue (App (Abs _ _) _) = false
+isValue _ = true
+
+stepEval :: Expr -> Expr
+stepEval (App (Abs x e) e') = replace x e' e
+  where
+    replace :: Id -> Expr -> Expr -> Expr
+    replace x e = go
+      where
+        go v@(Var y) = if x == y then e else v
+        go (App e' e'') = App (go e') (go e'')
+        go (Abs y e') = Abs y (go e') -- FIXME: avoid capture
+stepEval e = e
+
+
---
+++ src/Main.purs
@@ -0,0 +1,30 @@
+module Main where
+
+import Toy.Expression
+import Control.Monad
+import Control.Monad.Eff
+import Debug.Trace
+
+term1 :: Expr
+term1 = App (Abs "f" (App (App (Var "f") (Var "x")) (Var "y"))) (Abs "g" (App (Var "g") (Var "y")))
+
+diverge :: Expr
+diverge = App xToXX xToXX
+  where
+    xToXX = Abs "x" (App (Var "x") (Var "x"))
+
+interactiveEval :: forall e. Expr -> Eff (trace :: Trace | e) Unit
+interactiveEval e = do
+  trace "Starting evaluation:"
+  trace $ "0    " ++ show e
+  go 1 e
+  trace "Done evaluating!"
+    where
+      go :: forall e. Number -> Expr -> Eff (trace :: Trace | e) Unit
+      go n e = do
+        when (not $ isValue e) $ do
+          let next = stepEval e
+          trace $ show n ++ " => " ++ show next
+          go (n+1) next
+
+main = interactiveEval term1
